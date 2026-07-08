ea4f3499f750bc9cb7bb2ce9e62ba9347044a89a
mdaugavietis <martins.daugavitis@accenture.com>
2026-07-08 09:18:04 +0300

    Fix: remove unused classes

--- src/main/java/com/accenture/ems/emstraining/mapper/TrainingMapper.java
+++ src/main/java/com/accenture/ems/emstraining/mapper/TrainingMapper.java
@@ -11,6 +11,7 @@ import java.util.List;
 @Mapper(componentModel = "spring")
 public interface TrainingMapper {
     TrainingDTO toDTO(Training entity);
+
     List<TrainingDTO> toDTO(List<Training> entities);
 
     @Mapping(target = "id", ignore = true)
